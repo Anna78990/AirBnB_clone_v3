27384968c24b45627696b17e3fa303a3e8456e73
NeoV0id <neov0id.nova@gmail.com>
2022-05-22 08:48:30 -0700

    PEP8 for task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-""" """
+""" create a route /status on the object app_views
+    that returns a JSON: "status": "OK" """
 
 
 from api.v1.views import app_views
+
+
 @app_views.route('/status')
 def status():
     return ("\"status\": \"OK\"")
