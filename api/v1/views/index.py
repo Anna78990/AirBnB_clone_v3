ffc8f94547dabdc802b6aafa24a6c3332e7bb98b
Anna78990 <takasuanna1990@gmail.com>
2022-05-23 10:54:39 +0000

    pycode for task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,8 @@ index
 """
 from flask import jsonify
 from api.v1.views import app_views
-from models import storage 
+from models import storage
+
 
 @app_views.route('/status', strict_slashes=False)
 def status():
