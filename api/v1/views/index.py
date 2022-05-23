cc723a4594805f37af8281ceee1c828d8441b608
Anna78990 <takasuanna1990@gmail.com>
2022-05-23 10:30:27 +0000

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,19 @@ index
 """
 from flask import jsonify
 from api.v1.views import app_views
-
+from models import storage 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ return json object """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ return json object with the number of each objects """
+    classes = {"Amenity": 0, "City": 0,
+               "Place": 0, "Review": 0, "State": 0, "User": 0}
+    for k, v in classes.items():
+        classes[k] = storage.count(k)
+    return jsonify(classes)
