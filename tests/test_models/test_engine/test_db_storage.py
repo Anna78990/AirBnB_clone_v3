2afa4cbee428ba1288e9790819a2dcf5e7ce0ad6
NeoV0id <neov0id.nova@gmail.com>
2022-05-24 05:33:58 -0700

    Added get and count test for dbstorage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,11 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """Test to count objects in file storage"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_find(self):
+        """Test to find the objects of given id in file storage"""
